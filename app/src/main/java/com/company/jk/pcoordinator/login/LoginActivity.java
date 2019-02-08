683799f6dc053169fb036a6492969677d3d3e201
jungkuk jo <>
2019-02-08 10:37:04 +0900

    보드 개발 중

--- app/src/main/java/com/company/jk/pcoordinator/login/LoginActivity.java
+++ app/src/main/java/com/company/jk/pcoordinator/login/LoginActivity.java
@@ -145,22 +145,22 @@ public class LoginActivity extends MyActivity {
     }
 
 
-    public static String getKeyHash(final Context context) {
-        PackageInfo packageInfo = getPackageInfo(context, PackageManager.GET_SIGNATURES);
-        if (packageInfo == null)
-            return null;
-
-        for (Signature signature : packageInfo.signatures) {
-            try {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                return Base64.encodeToString(md.digest(), Base64.NO_WRAP);
-            } catch (NoSuchAlgorithmException e) {
-                Log.w(TAG, "Unable to get MessageDigest. signature=" + signature, e);
-            }
-        }
-        return null;
-    }
+//    public static String getKeyHash(final Context context) {
+//        PackageInfo packageInfo = getPackageInfo(context, PackageManager.GET_SIGNATURES);
+//        if (packageInfo == null)
+//            return null;
+//
+//        for (Signature signature : packageInfo.signatures) {
+//            try {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                return Base64.encodeToString(md.digest(), Base64.NO_WRAP);
+//            } catch (NoSuchAlgorithmException e) {
+//                Log.w(TAG, "Unable to get MessageDigest. signature=" + signature, e);
+//            }
+//        }
+//        return null;
+//    }
 
     public void OnClickMethod(View v) {      // TODO Auto-generated method stub
 
